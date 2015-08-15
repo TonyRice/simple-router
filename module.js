1990ae9ccd101df40b317ad04a93c21283b6e0a5
Tony Rice <tony@bestellensoftware.com>
2015-08-14 20:48:15 -0500

    Adding the project files

---
+++ module.js
@@ -0,0 +1,69 @@
+var Router = function () {
+
+    var getHandlers = {};
+    var postHandlers = {};
+    var putHandlers = {};
+    var deleteHandlers = {};
+    var defaultHandler = function () {
+        return {
+            entity: "",
+            content_type: "text/html"
+        }
+    };
+
+    this.get = function (regex, handler) {
+        var parsed = regex.toString().replace(/^\/|(\/|\/\g)$/g, '');
+        getHandlers[parsed] = handler;
+    };
+
+    this.post = function (regex, handler) {
+        var parsed = regex.toString().replace(/^\/|(\/|\/\g)$/g, '');
+        postHandlers[parsed] = handler;
+    };
+
+    this.put = function (regex, handler) {
+        var parsed = regex.toString().replace(/^\/|(\/|\/\g)$/g, '');
+        putHandlers[parsed] = handler;
+    };
+
+    this.delete = function (regex, handler) {
+        var parsed = regex.toString().replace(/^\/|(\/|\/\g)$/g, '');
+        deleteHandlers[parsed] = handler;
+    };
+
+    this.default = function (handler) {
+        defaultHandler = handler;
+    };
+
+    this.handle = function (req) {
+
+        var handlerGroup = {};
+
+        var method = req.method();
+
+        if (method === "GET") {
+            handlerGroup = getHandlers;
+        } else if (method === "POST") {
+            handlerGroup = postHandlers;
+        } else if (method === "PUT") {
+            handlerGroup = putHandlers;
+        } else if (method === "DELETE") {
+            handlerGroup = deleteHandlers;
+        }
+
+        var handled = false;
+        var handler = defaultHandler;
+
+        for (var r in handlerGroup) {
+            var reg = new RegExp(r);
+            if (reg.test(req.path())) {
+                handler = handlerGroup[r];
+                handled = true;
+                break;
+            }
+        }
+        handler(req, req.response());
+    };
+};
+
+module.exports = Router;
